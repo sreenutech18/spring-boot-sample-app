016049576b95c91161729449fad63a06367c662e
sreenutech18 <77761730+sreenutech18@users.noreply.github.com>
2023-07-01 10:25:15 +0530

    Update PingController.java

--- src/main/java/com/app/controller/PingController.java
+++ src/main/java/com/app/controller/PingController.java
@@ -13,10 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author z0043uwn
- */
 @RestController
 public class PingController {
 
