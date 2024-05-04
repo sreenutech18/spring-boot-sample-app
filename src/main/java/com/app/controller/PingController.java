0a59660fec4ee515cb9f028bc6565f0a7b2342dc
sreenutech18 <77761730+sreenutech18@users.noreply.github.com>
2024-05-04 17:41:28 +0530

    Update PingController.java

--- src/main/java/com/app/controller/PingController.java
+++ src/main/java/com/app/controller/PingController.java
@@ -21,7 +21,7 @@ public class PingController {
 
     @GetMapping("/ping/v1")
     public ResponseEntity<String> ping(HttpServletRequest request) {
-        return ResponseEntity.status(HttpStatus.OK).body("#####Sreenu Technologies For Java Realtime Projects Course*****");
+        return ResponseEntity.status(HttpStatus.OK).body("Welcome SreenuTech - Java Projects");
     }
 
 }
