32c89704ca952ac363f7a10f7a00ae074f6a7d2d
sreenutech18 <77761730+sreenutech18@users.noreply.github.com>
2026-01-15 09:06:36 +0530

    Update ping endpoint from v8 to v9

--- src/main/java/com/app/controller/PingController.java
+++ src/main/java/com/app/controller/PingController.java
@@ -19,7 +19,7 @@ public class PingController {
     @Autowired
     private PingService pingService;
 
-    @GetMapping("/ping/v8")
+    @GetMapping("/ping/v9")
     public ResponseEntity<String> ping(HttpServletRequest request) {
         return ResponseEntity.status(HttpStatus.OK).body("SREENU TECHNOLOGIES - JAVA REALTIME PROJECTS");
     }
